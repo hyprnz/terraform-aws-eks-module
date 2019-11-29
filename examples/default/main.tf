33616765fcbd706e9a7be39b2281749f7a1a878e
Phil Rodrigues <phil@hypr.co.nz>
2019-11-29 15:30:53 +1300

    Upgrade to terraform 0.12 , terraform 0.11 format is still available in branch "0.11" - versions

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -6,7 +6,7 @@ terraform {
 module "example" {
   source = "../../"
 
-  cluster_name                          = "eks-env-example"
+  cluster_name                          = "eks-stage-example"
   k8s_version                           = 1.13
   vpc_id                                = aws_vpc.test_vpc.id
   eks_master_subnet_ids                 = [aws_subnet.private1.id, aws_subnet.private2.id, aws_subnet.private3.id, aws_subnet.public1.id, aws_subnet.public2.id, aws_subnet.public3.id]
