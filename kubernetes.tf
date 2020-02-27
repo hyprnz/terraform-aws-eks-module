55b49a5d68e7b6fdf3116c73420f507ad02b283b
Reuben Dunn <reuben.dunn@hypr.co.nz>
2020-02-27 13:19:12 +1300

    Pinned K8 provider to 1.10 not 1.11 to fix #2 for 0.11 branch

--- kubernetes.tf
+++ kubernetes.tf
@@ -1,5 +1,5 @@
 provider "kubernetes" {
-  version                = "1.11"
+  version                = "1.10"
   host                   = "${aws_eks_cluster.this.endpoint}"
   cluster_ca_certificate = "${base64decode(aws_eks_cluster.this.certificate_authority.0.data)}"
   token                  = "${data.aws_eks_cluster_auth.this.token}"
